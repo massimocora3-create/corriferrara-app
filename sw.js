464f036cec835b4faaab43f045de5a917c4a349e
massimocora3-create <massimocora3@gmail.com>
2025-11-28 13:13:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("corriferrara-cache").then(cache => {
+      return cache.addAll([
+        "/corriferrara-app/",
+        "/corriferrara-app/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
