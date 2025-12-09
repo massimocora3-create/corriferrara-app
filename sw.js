a8cc463e5dda660a257a5334226015d8cc8d791e
massimocora3-create <massimocora3@gmail.com>
2025-12-09 09:57:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,49 @@
+const CACHE_NAME = "corriferrara-cache-v3";
+
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./biblioteca.html"
+];
+
+// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("corriferrara-cache-v2").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys
-          .filter(key => key !== "corriferrara-cache-v2")
-          .map(key => caches.delete(key))
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       );
     })
   );
   self.clients.claim();
 });
 
+// FETCH (NETWORK FIRST - SEMPRE AGGIORNATO)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
